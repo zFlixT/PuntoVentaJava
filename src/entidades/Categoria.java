6ad698ffc69126113ce6a93eb3c8ee93fa7530fe
zFlixT <pt0109032023@unab.edu.sv>
2025-03-02 15:20:58 -0600

    Realización de la guia 7: Creando la capa entidades

---
+++ src/entidades/Categoria.java
@@ -0,0 +1,55 @@
+package entidades;
+
+public class Categoria {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private boolean activo;
+
+    public Categoria(int id, String nombre, String descripcion, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", activo=" + activo + '}';
+    }
+
+    public Categoria() {
+    }
+}
